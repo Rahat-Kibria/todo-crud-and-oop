6a7f772f53054cd546136ebddc218d8b5eaaedf8
Rahat-Kibria <rahatkibriawahee@gmail.com>
2023-08-17 09:47:32 +0600

    first commit

---
+++ resources/views/progenitor.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo CRUD</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+    <link rel="stylesheet" href="{{ url('css/styles.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
